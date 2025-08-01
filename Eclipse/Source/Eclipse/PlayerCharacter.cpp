4c12491954ce035a0d85f7ed1a9b483acb82322e
YJ0717 <cafricool3@naver.com>
2025-08-02 08:07:36 +0900

    몽타주 실행시 플레이어 움직임 막기

--- Eclipse/Source/Eclipse/PlayerCharacter.cpp
+++ Eclipse/Source/Eclipse/PlayerCharacter.cpp
@@ -1,6 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "PlayerCharacter.h"
 #include "Components/CapsuleComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
@@ -14,36 +13,28 @@
 #include "DrawDebugHelpers.h"
 #include "Kismet/KismetSystemLibrary.h"
 
-// 기본값 설정
 APlayerCharacter::APlayerCharacter()
 {
-	// 매 프레임 Tick()을 호출하도록 이 캐릭터를 설정합니다. 필요하지 않은 경우 성능 향상을 위해 이 기능을 끌 수 있습니다.
 	PrimaryActorTick.bCanEverTick = true;
 
-	// 컨트롤러 회전을 사용하지 않습니다. 카메라가 컨트롤러 회전에 영향을 받지 않도록 합니다.
 	bUseControllerRotationPitch = false;
 	bUseControllerRotationYaw = false;
 	bUseControllerRotationRoll = false;
 
-	// 캐릭터가 이동 방향으로 회전하도록 설정합니다.
-	GetCharacterMovement()->bOrientRotationToMovement = true; 	
-	// 회전 속도를 설정합니다.
+	GetCharacterMovement()->bOrientRotationToMovement = true;
 	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f);
 
-	// 카메라 붐(카메라를 플레이어로부터 일정 거리에 유지)을 생성합니다.
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->TargetArmLength = 400.0f; // 카메라가 플레이어를 따라가는 거리
-	CameraBoom->bUsePawnControlRotation = true; // 컨트롤러를 기반으로 암을 회전합니다.
+	CameraBoom->TargetArmLength = 400.0f;
+	CameraBoom->bUsePawnControlRotation = true;
 
-	// 따라가는 카메라를 생성합니다.
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
-	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // 카메라를 붐의 끝에 부착합니다.
-	FollowCamera->bUsePawnControlRotation = false; // 카메라가 암을 기준으로 회전하지 않도록 합니다.
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false;
 
-	// 도끼 컴포넌트를 생성합니다.
 	AxeComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Axe"));
-	AxeComponent->SetupAttachment(GetMesh(), FName("AxeSocket")); // 초기에는 등 소켓에 부착
+	AxeComponent->SetupAttachment(GetMesh(), FName("AxeSocket"));
 	AxeComponent->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
 	AxeComponent->SetCollisionObjectType(ECollisionChannel::ECC_WorldDynamic);
 	AxeComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Block);
@@ -52,12 +43,10 @@ APlayerCharacter::APlayerCharacter()
 	bIsWeaponEquipped = false;
 }
 
-// 게임이 시작되거나 스폰될 때 호출됩니다.
 void APlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// 기본 매핑 컨텍스트를 추가합니다.
 	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
 	{
 		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
@@ -67,31 +56,18 @@ void APlayerCharacter::BeginPlay()
 	}
 }
 
-// 입력을 기능에 바인딩하기 위해 호출됩니다.
 void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-	// Enhanced Input 컴포넌트를 설정합니다.
 	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
 	{
-		// 점프
 		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ACharacter::Jump);
 		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
-
-		// 이동
 		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Move);
-
-		// 둘러보기
 		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Look);
-
-		// 무기 장착/해제
 		EnhancedInputComponent->BindAction(EquipAction, ETriggerEvent::Triggered, this, &APlayerCharacter::ToggleWeapon);
-
-		// 공격
 		EnhancedInputComponent->BindAction(AttackAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Attack);
-
-		// 구르기
 		EnhancedInputComponent->BindAction(DodgeAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Dodge);
 	}
 }
@@ -99,72 +75,58 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 void APlayerCharacter::ToggleWeapon()
 {
     UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
-    if (AnimInstance && (AnimInstance->Montage_IsPlaying(EquipMontage) || AnimInstance->Montage_IsPlaying(UnequipMontage)))
-    {
-        return;
-    }
+    if (!AnimInstance || AnimInstance->IsAnyMontagePlaying()) return;
 
-    if (bIsWeaponEquipped)
+    UAnimMontage* MontageToPlay = bIsWeaponEquipped ? UnequipMontage : EquipMontage;
+    if (MontageToPlay)
     {
-        // 무기 해제
-        if (UnequipMontage)
+        float PlayRate = AnimInstance->Montage_Play(MontageToPlay);
+        if (PlayRate > 0.f)
         {
-            AnimInstance->Montage_Play(UnequipMontage);
-            // 몽타주 길이를 가져와서 타이머 설정
-            const float MontageLength = UnequipMontage->GetPlayLength();
-            GetWorldTimerManager().SetTimer(UnequipTimerHandle, this, &APlayerCharacter::FinishUnequip, MontageLength, false);
-        }
-    }
-    else
-    {
-        // 무기 장착
-        if (EquipMontage)
-        {
-            AnimInstance->Montage_Play(EquipMontage);
-            bIsWeaponEquipped = true;
-            AxeComponent->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetNotIncludingScale, FName("HandSocket"));
+            GetCharacterMovement()->SetMovementMode(EMovementMode::MOVE_None);
+
+            FOnMontageEnded MontageEndedDelegate;
+            MontageEndedDelegate.BindUObject(this, &APlayerCharacter::OnMontageEnded);
+            AnimInstance->Montage_SetEndDelegate(MontageEndedDelegate, MontageToPlay);
+
+            // 몽타주 재생이 끝난 후 상태를 변경합니다.
+            if (bIsWeaponEquipped)
+            {
+                // Unequip
+                bIsWeaponEquipped = false;
+                AxeComponent->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetNotIncludingScale, FName("AxeSocket"));
+            }
+            else
+            {
+                // Equip
+                bIsWeaponEquipped = true;
+                AxeComponent->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetNotIncludingScale, FName("HandSocket"));
+            }
         }
     }
 }
 
-void APlayerCharacter::FinishUnequip()
-{
-    bIsWeaponEquipped = false;
-    AxeComponent->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetNotIncludingScale, FName("AxeSocket"));
-    GetWorldTimerManager().ClearTimer(UnequipTimerHandle);
-}
-
-// 이동 입력을 처리하는 함수
 void APlayerCharacter::Move(const FInputActionValue& Value)
 {
-	// 입력 벡터
 	FVector2D MovementVector = Value.Get<FVector2D>();
 
 	if (Controller != nullptr)
 	{
-		// 컨트롤러의 회전값을 가져옵니다.
 		const FRotator Rotation = Controller->GetControlRotation();
 		const FRotator YawRotation(0, Rotation.Yaw, 0);
-
-		// 전방 및 오른쪽 방향을 계산합니다.
 		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
 		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
-
-		// 이동 입력을 추가합니다.
 		AddMovementInput(ForwardDirection, MovementVector.Y);
 		AddMovementInput(RightDirection, MovementVector.X);
 	}
 }
 
-// 둘러보기 입력을 처리하는 함수
 void APlayerCharacter::Look(const FInputActionValue& Value)
 {
-	// 입력 벡터
 	FVector2D LookAxisVector = Value.Get<FVector2D>();
 
 	if (Controller != nullptr)
 	{
-		// 컨트롤러에 Yaw 및 Pitch 입력을 추가합니다.
 		AddControllerYawInput(LookAxisVector.X);
 		AddControllerPitchInput(LookAxisVector.Y);
 	}
@@ -175,12 +137,20 @@ void APlayerCharacter::Attack(const FInputActionValue& Value)
 	if (bIsWeaponEquipped)
 	{
 		UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
-		if (AnimInstance && AttackMontage && !AnimInstance->Montage_IsPlaying(AttackMontage))
+		if (AnimInstance && AttackMontage && !AnimInstance->IsAnyMontagePlaying())
 		{
-			AnimInstance->Montage_Play(AttackMontage);
-			// 공격 시작 시, 현재 프레임의 소켓 위치를 "이전 위치"로 저장합니다.
-			PreviousBladeBaseLocation = AxeComponent->GetSocketLocation(TEXT("BladeBaseSocket"));
-			PreviousBladeTipLocation = AxeComponent->GetSocketLocation(TEXT("BladeTipSocket"));
+            float PlayRate = AnimInstance->Montage_Play(AttackMontage);
+            if (PlayRate > 0.f)
+            {
+                GetCharacterMovement()->SetMovementMode(EMovementMode::MOVE_None);
+
+                FOnMontageEnded MontageEndedDelegate;
+                MontageEndedDelegate.BindUObject(this, &APlayerCharacter::OnMontageEnded);
+                AnimInstance->Montage_SetEndDelegate(MontageEndedDelegate, AttackMontage);
+
+                PreviousBladeBaseLocation = AxeComponent->GetSocketLocation(TEXT("BladeBaseSocket"));
+                PreviousBladeTipLocation = AxeComponent->GetSocketLocation(TEXT("BladeTipSocket"));
+            }
 		}
 	}
 }
@@ -190,26 +160,33 @@ void APlayerCharacter::Dodge(const FInputActionValue& Value)
 	UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
 	if (AnimInstance && DodgeMontage && !AnimInstance->IsAnyMontagePlaying())
 	{
-		// 마지막으로 입력된 이동 방향을 가져옵니다.
-		FVector LastInputDirection = GetCharacterMovement()->GetLastInputVector().GetSafeNormal();
+        FVector LastInputDirection = GetCharacterMovement()->GetLastInputVector().GetSafeNormal();
+        if (LastInputDirection.IsNearlyZero())
+        {
+            LastInputDirection = GetActorForwardVector();
+        }
 
-		// 만약 이동 입력이 없었다면, 캐릭터의 정면 방향으로 구릅니다.
-		if (LastInputDirection.IsNearlyZero())
-		{
-			LastInputDirection = GetActorForwardVector();
-		}
+        FRotator DodgeRotation = LastInputDirection.Rotation();
+        SetActorRotation(DodgeRotation);
 
-		// 구르기 애니메이션 재생
-		AnimInstance->Montage_Play(DodgeMontage);
+        float PlayRate = AnimInstance->Montage_Play(DodgeMontage);
+        if (PlayRate > 0.f)
+        {
+            GetCharacterMovement()->SetMovementMode(EMovementMode::MOVE_None);
 
-		// 캐릭터를 해당 방향으로 순간적으로 날려 보냅니다.
-		float DodgeDistance = 600.0f;
-		LaunchCharacter(LastInputDirection * DodgeDistance, true, true);
+            FOnMontageEnded MontageEndedDelegate;
+            MontageEndedDelegate.BindUObject(this, &APlayerCharacter::OnMontageEnded);
+            AnimInstance->Montage_SetEndDelegate(MontageEndedDelegate, DodgeMontage);
+        }
 	}
 }
 
+void APlayerCharacter::OnMontageEnded(UAnimMontage* Montage, bool bInterrupted)
+{
+	// 몽타주가 종료되면(중단 포함), 캐릭터의 움직임 모드를 다시 걷기 상태로 되돌립니다.
+	GetCharacterMovement()->SetMovementMode(EMovementMode::MOVE_Walking);
+}
 
-// 매 프레임 호출됩니다.
 void APlayerCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
@@ -217,7 +194,6 @@ void APlayerCharacter::Tick(float DeltaTime)
 	UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
 	if (bIsWeaponEquipped && AnimInstance && AnimInstance->Montage_IsPlaying(AttackMontage))
 	{
-		// 현재 프레임의 도끼 날 소켓 위치를 가져옵니다.
 		FVector CurrentBladeBaseLocation = AxeComponent->GetSocketLocation(TEXT("BladeBaseSocket"));
 		FVector CurrentBladeTipLocation = AxeComponent->GetSocketLocation(TEXT("BladeTipSocket"));
 
@@ -228,10 +204,8 @@ void APlayerCharacter::Tick(float DeltaTime)
 		ObjectTypes.Add(UEngineTypes::ConvertToObjectType(ECollisionChannel::ECC_WorldStatic));
 		ObjectTypes.Add(UEngineTypes::ConvertToObjectType(ECollisionChannel::ECC_Pawn));
 
-		// 각 소켓 위치에 대한 충돌 결과를 저장할 변수
 		FHitResult BaseHitResult, TipHitResult;
 
-		// 1. 손잡이(Base) 부분 충돌 감지 (디버그 라인 표시)
 		bool bBaseHit = UKismetSystemLibrary::SphereTraceSingleForObjects(
 			GetWorld(),
 			PreviousBladeBaseLocation,
@@ -245,7 +219,6 @@ void APlayerCharacter::Tick(float DeltaTime)
 			true
 		);
 
-		// 2. 도끼 날(Tip) 부분 충돌 감지 (디버그 라인 없음)
 		bool bTipHit = UKismetSystemLibrary::SphereTraceSingleForObjects(
 			GetWorld(),
 			PreviousBladeTipLocation,
@@ -259,10 +232,8 @@ void APlayerCharacter::Tick(float DeltaTime)
 			true
 		);
 
-		// 두 충돌 중 하나라도 발생했다면
 		if (bBaseHit || bTipHit)
 		{
-			// 실제 충돌이 일어난 결과를 사용합니다. (손잡이 부분이 우선)
 			const FHitResult& HitResult = bBaseHit ? BaseHitResult : TipHitResult;
 			AActor* HitActor = HitResult.GetActor();
 
@@ -278,7 +249,6 @@ void APlayerCharacter::Tick(float DeltaTime)
 			}
 		}
 
-		// 현재 프레임의 위치를 다음 프레임을 위해 "이전 위치"로 저장합니다.
 		PreviousBladeBaseLocation = CurrentBladeBaseLocation;
 		PreviousBladeTipLocation = CurrentBladeTipLocation;
 	}
--- Eclipse/Source/Eclipse/PlayerCharacter.h
+++ Eclipse/Source/Eclipse/PlayerCharacter.h
@@ -11,6 +11,7 @@ class UInputMappingContext;
 class UInputAction;
 class USpringArmComponent;
 class UCameraComponent;
+class UAnimMontage;
 
 UCLASS()
 class ECLIPSE_API APlayerCharacter : public ACharacter
@@ -18,107 +19,73 @@ class ECLIPSE_API APlayerCharacter : public ACharacter
 	GENERATED_BODY()
 
 public:
-	// 이 캐릭터의 속성에 대한 기본값을 설정합니다.
 	APlayerCharacter();
 
 protected:
-	// 게임이 시작되거나 스폰될 때 호출됩니다.
 	virtual void BeginPlay() override;
-	// 입력을 기능에 바인딩하기 위해 호출됩니다.
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-	// 이동 입력을 위해 호출됩니다.
-	void Move(const FInputActionValue& Value);
-
-	// 둘러보기 입력을 위해 호출됩니다.
-	void Look(const FInputActionValue& Value);
-
-	// 공격 입력을 위해 호출됩니다.
-	void Attack(const FInputActionValue& Value);
-
-	// 구르기 입력을 위해 호출됩니다.
-	void Dodge(const FInputActionValue& Value);
-
-public:	
-	// 매 프레임 호출됩니다.
 	virtual void Tick(float DeltaTime) override;
 
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+	void Attack(const FInputActionValue& Value);
+	void Dodge(const FInputActionValue& Value);
+
+	UFUNCTION()
+	void OnMontageEnded(UAnimMontage* Montage, bool bInterrupted);
+
 protected:
-	// 입력 매핑 컨텍스트
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	UInputMappingContext* DefaultMappingContext;
 
-	// 점프 입력 액션
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	UInputAction* JumpAction;
 
-	// 이동 입력 액션
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	UInputAction* MoveAction;
 
-	// 둘러보기 입력 액션
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	UInputAction* LookAction;
 
-	// 무기 장착 입력 액션
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	UInputAction* EquipAction;
 
-	// 공격 입력 액션
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	UInputAction* AttackAction;
 
-	// 구르기 입력 액션
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	UInputAction* DodgeAction;
 
-	// 캐릭터 뒤에 카메라를 배치하는 카메라 붐
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	USpringArmComponent* CameraBoom;
 
-	// 따라가는 카메라
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	UCameraComponent* FollowCamera;
 
-	// 도끼 컴포넌트
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Weapon, meta = (AllowPrivateAccess = "true"))
 	UStaticMeshComponent* AxeComponent;
 
-	// 무기 장착 상태
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Weapon)
 	bool bIsWeaponEquipped;
 
-	// 무기 장착/해제 함수
 	UFUNCTION(BlueprintCallable, Category = Weapon)
 	void ToggleWeapon();
 
-	// 무기 장착 애니메이션 몽타주
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Animation)
 	UAnimMontage* EquipMontage;
 
-	// 무기 해제 애니메이션 몽타주
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Animation)
 	UAnimMontage* UnequipMontage;
 
-	// 공격 애니메이션 몽타주
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Animation)
 	UAnimMontage* AttackMontage;
 
-	// 구르기 애니메이션 몽타주
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Animation)
 	UAnimMontage* DodgeMontage;
 
-	// 벽 충돌 시 생성될 파티클 이펙트
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Effects, meta = (AllowPrivateAccess = "true"))
 	UParticleSystem* ImpactEffect;
 
-	// 이전 프레임의 도끼 날 위치 (소켓 기반)
-	FVector PreviousBladeBaseLocation;
+		FVector PreviousBladeBaseLocation;
 	FVector PreviousBladeTipLocation;
-
-	// 무기 해제 완료 타이머 핸들
-	FTimerHandle UnequipTimerHandle;
-
-	// 무기 해제 완료 시 호출될 함수
-	void FinishUnequip();
 };
